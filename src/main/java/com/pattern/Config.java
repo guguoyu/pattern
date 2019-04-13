27161444e7852841da3def365490bcf094fa1ef3
guguoyu <guguoyu1@126.com>
2019-04-13 18:50:51 +0800

    123

---
+++ src/main/java/com/pattern/Config.java
@@ -0,0 +1,16 @@
+package com.pattern;
+
+import lombok.Data;
+
+/**
+ * @author guguoyu
+ * @date 2019/4/13
+ * @since JDK 1.8
+ */
+@Data
+public class Config {
+
+    private String className;
+
+    private String beanName;
+}
